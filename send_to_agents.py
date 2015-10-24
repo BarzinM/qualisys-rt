8650f1b8cdfdaf59bb53275446b240d188a73b16
BarzinM <Barzin.M@gmail.com>
2015-10-23 21:18:39 -0400

    Refactored the structure of send_to_agent executable and qtm class.

--- send_to_agents.py
+++ send_to_agents.py
@@ -11,6 +11,101 @@ logging.basicConfig(filename='send_to_agents.log', format=50 * '=' +
                     '\n%(asctime)s %(message)s', level=logging.DEBUG)
 
 
+def getIP():
+    # Get the IP address of the computer executing this file
+    temp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    temp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    temp_sock.connect(('192.168.0.1', 0))
+    ip_address = temp_sock.getsockname()[0]
+    temp_sock.close()
+    return ip_address
+
+
+class QualisysNav(object):
+
+    def __init__(self, number_of_bodies):
+        self.qt = qtm.QTMClient()
+        self.qt.setup()
+        self.number_of_bodies = number_of_bodies
+        self.messages = [qtm.body()] * number_of_bodies
+        self.conditions = [threading.Condition()] * number_of_bodies
+        pass
+
+    def run(self):
+        # Setting up server
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        server_address = (getIP(), 1895)
+        print >>sys.stderr, 'starting up on %s port %s' % server_address
+        sock.bind(server_address)
+        sock.listen(self.number_of_bodies)
+        print 'Ready for agents.'
+
+        # start receiving location of bodies from qualisys camera system
+        reciever = threading.Thread(name='reciever', target=self.recieveLocations)
+        reciever.daemon = True
+        reciever.start()
+        while True:
+            try:
+                print 'Waiting For Connection.'
+                connection, address = sock.accept()
+                print connection.getpeername(), 'connected'
+
+                # setup a new thread for sending to a new agent
+                thrd = threading.Thread(target=self.sendLocation, args=(connection,))
+                thrd.daemon = True
+                thrd.start()
+                print 'Number of running threads:',threading.activeCount()
+
+            except KeyboardInterrupt, e:
+                print '\nProgram Terminated by User.'
+                break
+
+            except Exception, e:
+                logging.exception(e)
+                raise
+
+    def recieveLocations(self):
+        while True:
+            try:
+                self.qt.getAttitude()
+            except socket.error, e:
+                logging.exception(e)
+                if e[0] == errno.EBADF:
+                    print 'Qualisys Getting Thread Terminated.'
+                raise
+            except Exception, e:
+                logging.exception(e)
+                raise
+            for i in range(self.number_of_bodies):
+                with self.conditions[i]:
+                    self.messages[i] = self.qt.bodies[i]
+                    self.conditions[i].notifyAll()
+
+    def sendLocation(self, connection):
+        # connection configuration
+        self.peername = connection.getpeername()
+        connection.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        try:
+            while True:
+                # recieve the body_id that is requested by agent
+                body_id = int(connection.recv(1))
+
+                # send the state of the body with ID specified in body_id
+                with self.conditions[body_id]:
+                    self.conditions[body_id].wait()
+                    msg = self.messages[body_id].pack()
+                connection.sendall(msg)
+        except ValueError, e:
+            print 'Connection terminated by', self.peername
+            sys.exit()
+        except Exception, e:
+            logging.exception(e)
+            print 'Error in sendToAgent():', e
+            raise
+
+
 def lineno():
     return inspect.currentframe().f_back.f_lineno
 
@@ -19,7 +114,6 @@ class get6D(threading.Thread):
 
     def __init__(self, qtm_obj):
         threading.Thread.__init__(self)
-        # self.condition = condition
         self.qt = qtm_obj
         self.count = len(self.qt.bodies)
 
@@ -37,7 +131,6 @@ class get6D(threading.Thread):
                 print 'Line @', lineno(), 'Error in get6D:', e
                 raise
                 sys.exit(e)
-            # print '--------------------'
             pose_data_buffer = struct.pack('<B', 2)
             for i in range(self.count):
                 pose_data_buffer += struct.pack('<f',
@@ -56,9 +149,6 @@ class get6D(threading.Thread):
                 conditions.acquire()
                 if not messages[i]:
                     messages[i] = pose_data_buffer
-                # if self.qt.bodies[i].linear_x!=struct.unpack('<f', pose_data_buffer[1:5])[0]:
-                #     print ' found one'
-                # print i, self.qt.bodies[i].angular_x, self.qt.bodies[i].angular_z
                 conditions.notify()
                 conditions.release()
 
@@ -67,10 +157,7 @@ class sendToAgent(threading.Thread):
 
     def __init__(self, index):
         threading.Thread.__init__(self)
-        # self.connection = connection
         self.index = index
-        # messages = msg
-        # self.conditions = conditions
         self.peername = connections_list[index].getpeername()
         connections_list[index].setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
@@ -106,68 +193,60 @@ class sendToAgent(threading.Thread):
         else:
             logging.exception(Exception)
 
-
-def getIP():
-    temp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    temp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    temp_sock.connect(('192.168.0.1', 0))
-    ip_address = temp_sock.getsockname()[0]
-    temp_sock.close()
-    return ip_address
+if __name__ == '__main__':
+    nav = QualisysNav(4)
+    nav.run()
 
 
-with qtm.QTMClient() as qt:
-    qt.setup()
-    number_of_bodies = len(qt.bodies)
+# with qtm.QTMClient() as qt:
+#     qt.setup()
+#     number_of_bodies = len(qt.bodies)
 
-    # Setting up server
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-    server_address = (getIP(), 1895)
-    print >>sys.stderr, 'starting up on %s port %s' % server_address
-    sock.bind(server_address)
+# Setting up server
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#     sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+#     server_address = (getIP(), 1895)
+#     print >>sys.stderr, 'starting up on %s port %s' % server_address
+#     sock.bind(server_address)
 
-    # Setting up recieving thread
-    conditions = threading.Condition()
-    messages = [[]] * 4
-    reciever = get6D(qt)
-    reciever.daemon = True
-    reciever.start()
+# Setting up recieving thread
+#     conditions = threading.Condition()
+#     messages = [[]] * 4
+#     reciever = get6D(qt)
+#     reciever.daemon = True
+#     reciever.start()
 
-    # Initialization for connection threads
-    sock.listen(4)
-    threads_list = []
-    connections_list = [[]] * 4
-    while True:
-        try:
-            # Accepting connections from agents
-            print 'Waiting For Connection.'
+# Initialization for connection threads
+#     sock.listen(4)
+#     threads_list = []
+#     connections_list = [[]] * 4
+#     while True:
+#         try:
+# Accepting connections from agents
+#             print 'Waiting For Connection.'
 
-            connection, address = sock.accept()
-            agent_id = int(connection.getpeername()[0][8]) - 1
-            print connection.getpeername(), 'ID:', agent_id + 1, 'connected'
+#             connection, address = sock.accept()
+#             agent_id = int(connection.getpeername()[0][8]) - 1
+#             print connection.getpeername(), 'ID:', agent_id + 1, 'connected'
 
-            # agent_id = 0 # SHOULD BE REMOVED LATER
-            # print 'number of bodies sent by qualisys: ', len(messages)
+#             print 'agent id:', agent_id, 'connections list:', connections_list
+#             connections_list[agent_id] = connection
 
-            print 'agent id:', agent_id, 'connections list:', connections_list
-            connections_list[agent_id] = connection
+# Starting a new thread for each new connection
+#             threads_list.append(sendToAgent(agent_id))
+#             threads_list[-1].daemon = True
+#             threads_list[-1].start()
 
-            # Starting a new thread for each new connection
-            threads_list.append(sendToAgent(agent_id))
-            threads_list[-1].daemon = True
-            threads_list[-1].start()
+#         except KeyboardInterrupt, e:
+#             print '\nProgram Terminated by User.'
+#             break
+#             sys.exit(0)
 
-        except KeyboardInterrupt, e:
-            print '\nProgram Terminated by User.'
-            break
-            sys.exit(0)
+#         except Exception, e:
+#             logging.exception(e)
+#             raise
+#             sys.exit(e)
 
-        except Exception, e:
-            logging.exception(e)
-            raise
-            sys.exit(e)
-
-    sock.close()
-    sys.exit(0)
+#     sock.close()
+#     sys.exit(0)
